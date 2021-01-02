a252fd794ef20dbf3a540cb1dc3be0b27ccf2dbd
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2021-01-02 10:23:56 -0500

    delete models

--- api/models/testimonial_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../node_modules/mongoose');
-
-const testimonial_schema = new mongoose.Schema({
-    text: { type: String, required: true },
-    author: { type: String, required: true },
-});
-
-// Testimonial = mongoose.model("testimonial", testimonial_schema);
-// module.exports = Testimonial;
